e2e39452dd307ff43c80f7a72bfd20949a987612
leila <leiladoskumbay@gmail.com>
2021-02-06 15:37:27 +0600

    'init'

---
+++ week1/sample1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Week1
+{
+        class Program
+        {
+        public string DefangIPaddr(string address)
+        {
+            return new System.Text.StringBuilder(address).Replace(".", "[.]").ToString();
+        }
+    }
+
+}
